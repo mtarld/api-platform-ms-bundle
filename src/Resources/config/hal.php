023d6113a8bd8618458aa7ecfcbfcfce8b1ec492
Mathias Arlaud <mathias.arlaud@gmail.com>
2020-10-14 17:31:13 +0200

    Move services definition to PHP

---
+++ src/Resources/config/hal.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Mtarld\ApiPlatformMsBundle\DependencyInjection\Loader\Configurator;
+
+use ApiPlatform\Core\Serializer\JsonEncoder;
+use Mtarld\ApiPlatformMsBundle\Serializer\Hal\ApiResourceDenormalizer;
+use Mtarld\ApiPlatformMsBundle\Serializer\Hal\CollectionDenormalizer;
+use Mtarld\ApiPlatformMsBundle\Serializer\Hal\ConstraintViolationListDenormalizer;
+use Mtarld\ApiPlatformMsBundle\Serializer\Hal\ObjectDenormalizer;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $container): void {
+    $container->services()
+        ->defaults()
+            ->private()
+        ->set('api_platform_ms.hal.encoder', JsonEncoder::class)
+            ->args([
+                'jsonhal',
+            ])
+            ->tag('serializer.encoder')
+        ->set('api_platform_ms.hal.denormalizer.constraint_violation_list', ConstraintViolationListDenormalizer::class)
+            ->call('setNameConverter', [
+                service('api_platform.name_converter')->ignoreOnInvalid(),
+            ])
+            ->tag('serializer.normalizer', [
+                'priority' => -880, // Run before api_platform.hal.normalizer.item
+            ])
+        ->set('api_platform_ms.hal.denormalizer.collection', CollectionDenormalizer::class)
+            ->tag('serializer.normalizer', [
+                'priority' => -870, // Run before api_platform_ms.hal.denormalizer.object
+            ])
+        ->set('api_platform_ms.hal.denormalizer.api_resource', ApiResourceDenormalizer::class)
+            ->tag('serializer.normalizer', [
+                'priority' => -870, // Run before api_platform_ms.hal.denormalizer.object
+            ])
+        ->set('api_platform_ms.hal.denormalizer.object', ObjectDenormalizer::class)
+            ->tag('serializer.normalizer', [
+                'priority' => -880, // Run before api_platform.hal.normalizer.item
+            ])
+    ;
+};
---
+++ src/Resources/config/services.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Mtarld\ApiPlatformMsBundle\DependencyInjection\Loader\Configurator;
+
+use Mtarld\ApiPlatformMsBundle\ApiResource\ExistenceChecker;
+use Mtarld\ApiPlatformMsBundle\Collection\PaginatedCollectionIterator;
+use Mtarld\ApiPlatformMsBundle\Controller\ApiResourceExistenceCheckerAction;
+use Mtarld\ApiPlatformMsBundle\HttpClient\GenericHttpClient;
+use Mtarld\ApiPlatformMsBundle\Microservice\MicroservicePool;
+use Mtarld\ApiPlatformMsBundle\Routing\RouteLoader;
+use Mtarld\ApiPlatformMsBundle\Validator\ApiResourceExistValidator;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ReferenceConfigurator;
+
+return static function (ContainerConfigurator $container): void {
+    $container->services()
+        ->defaults()
+            ->private()
+        ->set('api_platform_ms.route_loader', RouteLoader::class)
+            ->args([
+                param('api_platform_ms.name'),
+                param('api_platform_ms.hosts'),
+            ])
+            ->tag('routing.route_loader')
+        ->set('api_platform_ms.microservice_pool', MicroservicePool::class)
+            ->args([
+                service('validator'),
+                param('api_platform_ms.microservices'),
+            ])
+        ->alias(MicroservicePool::class, 'api_platform_ms.microservice_pool')
+        ->set(ApiResourceExistenceCheckerAction::class, ApiResourceExistenceCheckerAction::class)
+            ->args([
+                service('serializer'),
+                service('api_platform.iri_converter'),
+                service('api_platform.validator'),
+            ])
+            ->tag('controller.service_arguments')
+        ->set('api_platform_ms.http_client.generic', GenericHttpClient::class)
+            ->args([
+                service('serializer'),
+                service('api_platform_ms.http_client'),
+            ])
+        ->alias(GenericHttpClient::class, 'api_platform_ms.http_client.generic')
+        ->set('api_platform_ms.api_resource.existence_checker', ExistenceChecker::class)
+            ->args([
+                service('api_platform_ms.http_client.generic'),
+                service('serializer'),
+                service('api_platform_ms.microservice_pool'),
+            ])
+        ->set('api_platform_ms.validator.api_resource_exist', ApiResourceExistValidator::class)
+            ->args([
+                service('api_platform_ms.api_resource.existence_checker'),
+            ])
+            ->call('setLogger', [
+                service('logger'),
+            ])
+            ->tag('validator.constraint_validator')
+        ->alias(ApiResourceExistValidator::class, 'api_platform_ms.validator.api_resource_exist')
+        ->set('api_platform_ms.collection.paginated_collection_iterator', PaginatedCollectionIterator::class)
+            ->args([
+                service('api_platform_ms.http_client.generic'),
+                service('serializer'),
+            ])
+        ->alias(PaginatedCollectionIterator::class, 'api_platform_ms.validator.api_resource_exist')
+    ;
+};
+
+function service(string $id): ReferenceConfigurator
+{
+    $fn = function_exists('Symfony\Component\DependencyInjection\Loader\Configurator\service')
+        ? 'Symfony\Component\DependencyInjection\Loader\Configurator\service'
+        : 'Symfony\Component\DependencyInjection\Loader\Configurator\ref'
+    ;
+
+    return ($fn)($id);
+}
+
+function param(string $name): string
+{
+    return function_exists('Symfony\Component\DependencyInjection\Loader\Configurator\param')
+        ? ('Symfony\Component\DependencyInjection\Loader\Configurator\param')($name)
+        : '%'.$name.'%'
+    ;
+}
