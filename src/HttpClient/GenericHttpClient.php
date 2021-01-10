44c6389d67bc71c0bd39eccc7ada467c1507566b
Mathias Arlaud <mathias.arlaud@gmail.com>
2021-01-10 13:39:09 +0100

    Improve wrapped http client replacement

--- src/HttpClient/GenericHttpClient.php
+++ src/HttpClient/GenericHttpClient.php
@@ -13,7 +13,7 @@ use Symfony\Contracts\HttpClient\ResponseInterface;
  *
  * @author Mathias Arlaud <mathias.arlaud@gmail.com>
  */
-class GenericHttpClient
+class GenericHttpClient implements ReplaceableHttpClientInterface
 {
     private const MIME_TYPES = [
         'jsonld' => 'application/ld+json',
@@ -58,11 +58,6 @@ class GenericHttpClient
         }
 
         return $this->httpClient->request($method, $uri, $options);
-    }
-
-    public function getWrappedHttpClient(): HttpClientInterface
-    {
-        return $this->httpClient;
     }
 
     public function setWrappedHttpClient(HttpClientInterface $httpClient): void
