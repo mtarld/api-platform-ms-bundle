a7b8a3a17ed970a8b0e5dbfb4c3374e69cc981be
ismail1432 <smaine.milianni@gmail.com>
2021-11-10 22:58:11 +0100

    update php-cs fixer

---
+++ .php-cs-fixer.php
@@ -0,0 +1,19 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__.'/{src,tests}')
+;
+
+$config = new PhpCsFixer\Config();
+
+return $config->setRules([
+    '@Symfony' => true,
+    'concat_space' => ['spacing' => 'none'],
+    'multiline_whitespace_before_semicolons' => ['strategy' => 'new_line_for_chained_calls'],
+    'array_syntax' => ['syntax' => 'short'],
+    'ordered_imports' => ['sort_algorithm' => 'alpha'],
+])
+    ->setRiskyAllowed(true)
+    ->setFinder($finder)
+    ->setCacheFile(__DIR__.'/.php_cs.cache')
+    ;
