3c5b4c0392cf53486130c47ded62bccdc353be96
Martin Meese <m.meese@artemiskliniken.de>
2023-04-03 17:22:18 +0200

    style: remove verbose code

--- tests/Fixtures/App/src/Entity/Puppy.php
+++ tests/Fixtures/App/src/Entity/Puppy.php
@@ -4,26 +4,12 @@ namespace Mtarld\ApiPlatformMsBundle\Tests\Fixtures\App\src\Entity;
 
 use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
-use ApiPlatform\Metadata\Delete;
-use ApiPlatform\Metadata\Get;
-use ApiPlatform\Metadata\GetCollection;
-use ApiPlatform\Metadata\Patch;
-use ApiPlatform\Metadata\Post;
-use ApiPlatform\Metadata\Put;
 use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @author Mathias Arlaud <mathias.arlaud@gmail.com>
  */
 #[ApiResource(
-    operations: [
-        new Delete(uriTemplate: 'puppies/{id}'),
-        new Get(uriTemplate: 'puppies/{id}'),
-        new Put(uriTemplate: 'puppies/{id}'),
-        new Patch(uriTemplate: 'puppies/{id}'),
-        new GetCollection(uriTemplate: 'puppies', itemUriTemplate: 'puppies/{id}'),
-        new Post(uriTemplate: 'puppies'),
-    ],
     normalizationContext: ['groups' => 'read']
 )]
 class Puppy
