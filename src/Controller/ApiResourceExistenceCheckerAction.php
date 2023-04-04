58dc1303f8ca6aa33ff83f109418cabe347f3693
Martin Meese <m.meese@artemiskliniken.de>
2023-04-04 10:27:26 +0200

    feat: remove deprecation

--- src/Controller/ApiResourceExistenceCheckerAction.php
+++ src/Controller/ApiResourceExistenceCheckerAction.php
@@ -33,7 +33,7 @@ class ApiResourceExistenceCheckerAction
 
     public function __invoke(Request $request): JsonResponse
     {
-        if (null === $contentType = $request->getContentType()) {
+        if (null === $contentType = $request->getContentTypeFormat()) {
             throw new BadRequestHttpException('Content type is not supported');
         }
 
